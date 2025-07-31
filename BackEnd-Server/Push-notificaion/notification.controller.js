811af71385a651b199795337be5b470aeb926ac4
BALAJI <balajimohan941@gmail.com>
2025-07-31 18:01:55 +0530

    Final fix with  PN Flow

--- BackEnd-Server/Push-notificaion/notification.controller.js
+++ BackEnd-Server/Push-notificaion/notification.controller.js
@@ -1,10 +1,15 @@
 // notification.controller.js
 const admin = require('firebase-admin');
+require('dotenv').config();
 
-const serviceAccount = require('./firebase-service-account.json');
+// const serviceAccount = require('./firebase-service-account.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
 });
 
 async function sendNotification(token, title, body) {
