931cdc12561462a6451621b611d888d6ad53df44
BALAJI <balajimohan941@gmail.com>
2025-08-02 17:29:01 +0530

    install cors

--- BackEnd-Server/Push-notificaion/notification.routes.js
+++ BackEnd-Server/Push-notificaion/notification.routes.js
@@ -1,7 +1,10 @@
 // notification.routes.js
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const { sendNotification } = require('./notification.controller');
+
+app.use(cors());
 
 router.post('/send-notification', async (req, res) => {
   const { token, title, body } = req.body;
