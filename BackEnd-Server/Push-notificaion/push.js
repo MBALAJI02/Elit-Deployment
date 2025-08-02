03f6f3448e1d01f972abe4f837570984f35f5317
BALAJI <balajimohan941@gmail.com>
2025-08-02 17:50:49 +0530

    added cors-2

--- BackEnd-Server/Push-notificaion/push.js
+++ BackEnd-Server/Push-notificaion/push.js
@@ -8,7 +8,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PN_PORT;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(bodyParser.json());
 
