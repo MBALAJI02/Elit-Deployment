1339e868665c447c5ef4f53a8c0b47f37a5b8de4
BALAJI <balajimohan941@gmail.com>
2025-08-07 17:46:57 +0530

    Commit - Fix global server

--- BackEnd-Server/OTP-RegisterationMain/otp-register-backend/server.js
+++ BackEnd-Server/OTP-RegisterationMain/otp-register-backend/server.js
@@ -9,11 +9,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:4200', // Angular port
-  methods: ['GET', 'POST'],
-  credentials: true // Optional if you're using cookies
-}));
+app.use(cors({}));
 
 app.use(bodyParser.json());
 
